71db055c648742f2b774f83fa2e9948f1537f340
gecko2308 <60891050+gecko2308@users.noreply.github.com>
2022-05-03 20:45:59 -0400

    Initial setup for supabase and creation of login component with story.

---
+++ stories/components/authentication/login.stories.jsx
@@ -0,0 +1,8 @@
+import Auth from "../../../components/authentication/login";
+
+export default {
+    title: "Components/Authentication",
+    component: Auth,
+};
+
+export const AuthComponent = () => <Auth />;
