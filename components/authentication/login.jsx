869813bca9b70345f383d983da463d93377c4272
Grégory Cogneau <gcogneau@gmail.com>
2022-05-03 22:05:39 -0400

    creation de sign-in sign-up authentification Facebook fonctionnelle
    Changes by ericledonge

--- components/authentication/login.jsx
+++ components/authentication/login.jsx
@@ -1,9 +1,10 @@
 import { useState } from 'react'
-import {supabase} from "../../utils/supabaseClient";
+import {supabase} from "../../libs/supabaseClient";
 
 export default function Auth() {
     const [loading, setLoading] = useState(false)
     const [email, setEmail] = useState('')
+
     const handleLogin = async (email) => {
         try {
             setLoading(true)
