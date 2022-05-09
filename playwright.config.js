e6ac764243f40bcdb954b9f0837cc223031e03f8
Eric Le Donge <eric.ledonge@gmail.com>
2022-05-08 21:36:28 -0400

    :construction_worker: baseUrl

--- playwright.config.js
+++ playwright.config.js
@@ -1,8 +1,10 @@
+console.log(process.env.PLAYWRIGHT_TEST_BASE_URL);
+
 const config = {
   testDir: "tests/e2e",
   timeout: 2000,
   use: {
-    baseURL: process.env.PLAYWRIGHT_TEST_BASE_URL,
+    baseURL: process.env.PLAYWRIGHT_TEST_BASE_URL || "http://localhost:3000",
   },
 };
 
