de5271729d6d4c9d0b5407b6bae86196c4e64572
Eric <eric.ledonge@gmail.com>
2022-05-08 21:51:59 -0400

    :construction_worker: CI

--- playwright.config.js
+++ playwright.config.js
@@ -1,6 +1,9 @@
 const config = {
   testDir: "tests/e2e",
-  timeout: 2000,
+  timeout: 5000,
+  use: {
+    baseURL: process.env.PLAYWRIGHT_TEST_BASE_URL || "http://localhost:3000",
+  },
 };
 
 module.exports = config;
