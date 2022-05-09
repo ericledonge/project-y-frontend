8fa302ffeef518304c52add0dd581c715114f9d3
Eric Le Donge <eric.ledonge@gmail.com>
2022-05-08 21:19:57 -0400

    :construction_worker: baseUrl

--- playwright.config.js
+++ playwright.config.js
@@ -1,6 +1,9 @@
 const config = {
   testDir: "tests/e2e",
   timeout: 2000,
+  use: {
+    baseURL: "http://localhost:3000/",
+  },
 };
 
 module.exports = config;
--- tests/e2e/connection.spec.ts
+++ tests/e2e/connection.spec.ts
@@ -2,7 +2,7 @@ import { test, expect } from "@playwright/test";
 
 test.describe("Home Page", () => {
   test.beforeEach(async ({ page }) => {
-    await page.goto("http://localhost:3000");
+    await page.goto("/");
   });
 
   test("should display Project Y as title", async ({ page }) => {
@@ -12,7 +12,7 @@ test.describe("Home Page", () => {
   test.describe("when the user clicks on Connection", () => {
     test(", it should display the Sign In page", async ({ page }) => {
       await page.locator("text=Connexion").click();
-      await page.waitForNavigation({ url: "http://localhost:3000/sign-in" });
+      await page.waitForNavigation({ url: "/sign-in" });
     });
   });
 });
