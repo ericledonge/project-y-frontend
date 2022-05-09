40bb424092ff9ab52a82bcb4ea60a41459d5d265
Eric Le Donge <eric.ledonge@gmail.com>
2022-05-08 21:49:03 -0400

    :construction_worker: baseUrl

--- tests/e2e/connection.spec.ts
+++ tests/e2e/connection.spec.ts
@@ -12,7 +12,7 @@ test.describe("Home Page", () => {
   test.describe("when the user clicks on Connection", () => {
     test(", it should display the Sign In page", async ({ page }) => {
       await page.locator("text=Connexion").click();
-      await page.waitForNavigation({ url: /sign-in/ });
+      await page.waitForURL(/sign-in/);
     });
   });
 });
